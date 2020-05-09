f787b5b265770a3d04f7922c1c8a3f6e8d7d16ab
eliassjogreen <eliassjogreen1@gmail.com>
2020-05-09 15:20:19 +0200

    std v0.50.0 support

--- deps.ts
+++ deps.ts
@@ -1,21 +1,21 @@
-export { parse } from "https://deno.land/std@v1.0.0-rc1/flags/mod.ts";
+export { parse } from "https://deno.land/std@v0.50.0/flags/mod.ts";
 export {
   exists,
   readFileStr,
-} from "https://deno.land/std@v1.0.0-rc1/fs/mod.ts";
+} from "https://deno.land/std@v0.50.0/fs/mod.ts";
 export {
   dirname,
   extname,
   globToRegExp,
   resolve,
-} from "https://deno.land/std@v1.0.0-rc1/path/mod.ts";
-export { deferred } from "https://deno.land/std@v1.0.0-rc1/util/async.ts";
+} from "https://deno.land/std@v0.50.0/path/mod.ts";
+export { deferred } from "https://deno.land/std@v0.50.0/async/mod.ts";
 export {
   green,
   red,
   setColorEnabled,
   yellow,
-} from "https://deno.land/std@v1.0.0-rc1/fmt/mod.ts";
+} from "https://deno.land/std@v0.50.0/fmt/mod.ts";
 export {
   grant,
-} from "https://deno.land/std@v1.0.0-rc1/permissions/mod.ts";
+} from "https://deno.land/std@v0.50.0/permissions/mod.ts";
