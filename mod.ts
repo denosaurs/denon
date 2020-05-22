a8fd2117611922fec0a7b4773f8023164ac6f1e8
Qu4k <pippo.reds@gmail.com>
2020-05-23 00:14:53 +0200

    fix(mod): export only the needed objects from config.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
 
 export * from "./denon.ts";
-export * from "./src/config.ts";
 export * from "./src/watcher.ts";
 export * from "./src/runner.ts";
+
+export { DenonConfig, DEFAULT_DENON_CONFIG } from "./src/config.ts";
