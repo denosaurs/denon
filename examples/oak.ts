50e36eb4a48c2a925a001b38fab75eb96e5be5d0
Qu4k <pippo.reds@gmail.com>
2020-05-22 23:21:09 +0200

    docs(examples): add oak example

---
+++ examples/oak.ts
@@ -0,0 +1,19 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { green } from "https://deno.land/std/fmt/colors.ts";
+
+const app = new Application();
+
+let PORT: number = 8000;
+
+let port = Deno.env.get("PORT");
+if (port) {
+  PORT = Number.parseInt(port);
+}
+
+app.use((ctx) => {
+  ctx.response.body = "Hello Denon!";
+});
+
+console.log("Listening to", green(`:${PORT}`));
+
+await app.listen({ port: 8000 });
