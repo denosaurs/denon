20ca84df67fcd32f5ae678f1d43333237fb0dca5
Qu4k <pippo.reds@gmail.com>
2020-05-22 15:53:34 +0200

    fix: change version number

--- denon.ts
+++ denon.ts
@@ -16,7 +16,7 @@ import { readConfig, DenonConfig } from "./src/config.ts";
 import { parseArgs } from "./src/args.ts";
 import { setupLog } from "./src/log.ts";
 
-const VERSION = "v1.10.0";
+const VERSION = "v2.0.0";
 
 /**
  * Events you can listen to when creating a `denon`
