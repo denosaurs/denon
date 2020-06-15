7096f33c12d3a45be9547d6b35eab0aba723b3e7
Qu4k <pippo.reds@gmail.com>
2020-06-15 19:43:23 +0200

    fix: bump deno stdlib

--- deps.ts
+++ deps.ts
@@ -1,13 +1,13 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
 
 // provide better logging, see src/log.ts
-export * as log from "https://deno.land/std@0.54.0/log/mod.ts";
-export { LogRecord } from "https://deno.land/std@0.54.0/log/logger.ts";
+export * as log from "https://deno.land/std@0.57.0/log/mod.ts";
+export { LogRecord } from "https://deno.land/std@0.57.0/log/logger.ts";
 export {
   LogLevels,
   LevelName as LogLevelName,
-} from "https://deno.land/std@0.54.0/log/levels.ts";
-export { BaseHandler } from "https://deno.land/std@0.54.0/log/handlers.ts";
+} from "https://deno.land/std@0.57.0/log/levels.ts";
+export { BaseHandler } from "https://deno.land/std@0.57.0/log/handlers.ts";
 
 // colors for a pretty cli
 export {
@@ -18,7 +18,7 @@ export {
   yellow,
   red,
   gray,
-} from "https://deno.land/std@0.54.0/fmt/colors.ts";
+} from "https://deno.land/std@0.57.0/fmt/colors.ts";
 
 // configuration reading
 export {
@@ -26,13 +26,13 @@ export {
   existsSync,
   readFileStr,
   walk, // ... and one type of file monitoring
-} from "https://deno.land/std@0.54.0/fs/mod.ts";
+} from "https://deno.land/std@0.57.0/fs/mod.ts";
 
 // configuration parsing (YAML)
 export {
   JSON_SCHEMA,
   parse as parseYaml,
-} from "https://deno.land/std@0.54.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.57.0/encoding/yaml.ts";
 
 // file watching and directory matching
 export {
@@ -41,17 +41,17 @@ export {
   extname,
   resolve,
   globToRegExp,
-} from "https://deno.land/std@0.54.0/path/mod.ts";
+} from "https://deno.land/std@0.57.0/path/mod.ts";
 
 // configuration parsing and writing (JSON)
-export { readJson } from "https://deno.land/std@0.54.0/fs/read_json.ts";
-export { writeJson } from "https://deno.land/std@0.54.0/fs/write_json.ts";
+export { readJson } from "https://deno.land/std@0.57.0/fs/read_json.ts";
+export { writeJson } from "https://deno.land/std@0.57.0/fs/write_json.ts";
 
 // event control
-export { deferred, delay } from "https://deno.land/std@0.54.0/async/mod.ts";
+export { deferred, delay } from "https://deno.land/std@0.57.0/async/mod.ts";
 
 // permission management
-export { grant } from "https://deno.land/std@0.54.0/permissions/mod.ts";
+export { grant } from "https://deno.land/std@0.57.0/permissions/mod.ts";
 
 // autocomplete
 export * as omelette from "https://deno.land/x/omelette/omelette.ts";
