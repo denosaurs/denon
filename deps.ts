9d84479b43141bef95868f350c4200cb45e8673b
eliassjogreen <eliassjogreen1@gmail.com>
2020-10-09 15:49:55 +0200

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -14,20 +14,20 @@ export {
   italic,
   red,
   gray,
-} from "https://deno.land/std@0.71.0/fmt/colors.ts";
+} from "https://deno.land/std@0.74.0/fmt/colors.ts";
 
 // configuration reading
 export {
   exists,
   existsSync,
   walk, // ... and one type of file monitoring
-} from "https://deno.land/std@0.71.0/fs/mod.ts";
+} from "https://deno.land/std@0.74.0/fs/mod.ts";
 
 // configuration parsing (YAML)
 export {
   JSON_SCHEMA,
   parse as parseYaml,
-} from "https://deno.land/std@0.71.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.74.0/encoding/yaml.ts";
 
 // file watching and directory matching
 export {
@@ -36,10 +36,10 @@ export {
   extname,
   resolve,
   globToRegExp,
-} from "https://deno.land/std@0.71.0/path/mod.ts";
+} from "https://deno.land/std@0.74.0/path/mod.ts";
 
 // event control
-export { deferred, delay } from "https://deno.land/std@0.71.0/async/mod.ts";
+export { deferred, delay } from "https://deno.land/std@0.74.0/async/mod.ts";
 
 // permission management
-export { grant } from "https://deno.land/std@0.71.0/permissions/mod.ts";
+export { grant } from "https://deno.land/std@0.74.0/permissions/mod.ts";
