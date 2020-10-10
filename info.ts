a39a74c36b062b4004722efb1af3eddd98a959cb
eliassjogreen <eliassjogreen1@gmail.com>
2020-10-10 14:33:07 +0200

    chore: update info.ts

--- info.ts
+++ info.ts
@@ -1,7 +1,7 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
 
-export const VERSION = "2.4.1";
-export const BRANCH = "master";
+export const VERSION = "2.4.2";
+export const BRANCH = "main";
 // export const COMPAT: { [denon: string]: string[] } = {
 //   "2.3.0": ["1.2.0"],
 //   "2.3.1": ["1.2.0", "1.2.1", "1.2.2"],
