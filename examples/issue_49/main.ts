f6fb3dab25f1f0ef30a92d7f38f34af159dee767
eliassjogreen <eliassjogreen1@gmail.com>
2020-10-14 11:35:07 +0200

    feat: issue 49 example

---
+++ examples/issue_49/main.ts
@@ -0,0 +1,9 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8080 });
