417ef7e5c9fb223a846568d86a90e35e645d17f4
eliassjogreen <eliassjogreen1@gmail.com>
2020-11-21 03:27:05 +0100

    chore: bump deps

--- info.ts
+++ info.ts
@@ -1,6 +1,6 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
 
-export const VERSION = "2.4.4";
+export const VERSION = "2.4.5";
 export const BRANCH = "main";
 // export const COMPAT: { [denon: string]: string[] } = {
 //   "2.3.0": ["1.2.0"],
